6a6c9556ffc3d70ce2613058870d83b52a7616ce
Qalxry <qalxry@qq.com>
2024-01-23 01:53:32 +0800

    Site updated: 2024-01-23 01:53:32

--- js/custom/mydetails.js
+++ js/custom/mydetails.js
@@ -63,26 +63,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
     console.log("[DEBUG] Finished the element replacement.");
   });
 
-  // When scroll to the mydetails element, add ".disable-lazyload-mathjax" class to it.
-  mydetailsElements.forEach(function (mydetails) {
-    var observer = new IntersectionObserver(
-      function (entries) {
-        entries.forEach(function (entry) {
-          if (entry.isIntersecting) {
-            // 为所有子类添加 “.disable-lazyload-mathjax” 类
-            mydetails.querySelectorAll("*").forEach((element) => {
-              element.classList.add("disable-lazyload-mathjax");
-            });
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      {
-        rootMargin: "0px 0px 200% 0px",
-      }
-    );
-    observer.observe(mydetails);
-  });
+  // // When scroll to the mydetails element, add ".disable-lazyload-mathjax" class to it.
+  // mydetailsElements.forEach(function (mydetails) {
+  //   var observer = new IntersectionObserver(
+  //     function (entries) {
+  //       entries.forEach(function (entry) {
+  //         if (entry.isIntersecting) {
+  //           // 为所有子类添加 “.disable-lazyload-mathjax” 类
+  //           mydetails.querySelectorAll("*").forEach((element) => {
+  //             element.classList.add("disable-lazyload-mathjax");
+  //           });
+  //           observer.unobserve(entry.target);
+  //         }
+  //       });
+  //     },
+  //     {
+  //       rootMargin: "0px 0px 200% 0px",
+  //     }
+  //   );
+  //   observer.observe(mydetails);
+  // });
 
   mydetailsElements.forEach(function (mydetails) {
     // 初始化内容高度为0
